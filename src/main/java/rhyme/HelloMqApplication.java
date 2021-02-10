57284013bfd4381e58a10103e8e53a95e14c4590
依韵 <763525854@qq.com>
2021-02-10 21:37:35 +0800

    整合rabbitmq

---
+++ src/main/java/rhyme/HelloMqApplication.java
@@ -0,0 +1,11 @@
+package rhyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMqApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMqApplication.class, args);
+	}
+}
